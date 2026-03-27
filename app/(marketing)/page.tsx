ef7242c59c53ebf51a5b1564ee2b80e073d07c03
johanrussouw <jrussouw610505@gmail.com>
2026-03-27 16:45:26 +0200

    made all changes

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+import { HeroHeader } from "@/app/(marketing)/_components/header";
+//import { ThemeToggle } from "../components/ui/theme-toggle";
+import HeroSection from "@/app/(marketing)/_components/hero-section";
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello Johan</h1>
+      <HeroHeader />
+
+      <HeroSection />
+    </div>
+  );
+}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ThemeToggle } from "../components/ui/theme-toggle";
-export default function Home() {
-  return (
-    <div>
-      <h1>Hello Johan</h1>
-      <ThemeToggle />
-    </div>
-  );
-}
