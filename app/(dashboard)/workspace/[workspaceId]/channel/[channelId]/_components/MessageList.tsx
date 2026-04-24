7aabd72df28e52ba1bd3b604d377e9681848b1ed
johanrussouw <jrussouw610505@gmail.com>
2026-04-24 14:01:05 +0200

    Finished message editing in list

--- app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/MessageList.tsx
+++ app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/MessageList.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { MessageItem } from "./message/MessageItem";
 import { orpc } from "@/lib/orpc";
 import { useParams } from "next/navigation";
@@ -46,6 +46,9 @@ export function MessageList() {
     staleTime: 30_000,
     refetchOnWindowFocus: false,
   });
+  const {
+    data: { user },
+  } = useSuspenseQuery(orpc.workspace.list.queryOptions());
   // scroll to the bottom when messages first load
   useEffect(() => {
     if (!hasInitialScrolled && data?.pages.length) {
@@ -170,7 +173,11 @@ export function MessageList() {
           </div>
         ) : (
           items?.map((message) => (
-            <MessageItem key={message.id} message={message} />
+            <MessageItem
+              key={message.id}
+              message={message}
+              currentUserId={user.id}
+            />
           ))
         )}
         <div ref={bottomRef}></div>
